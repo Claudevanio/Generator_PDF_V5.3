fcc3369ac60d6e3762103ca2d026a1986cf03e51
Claudevanio <van331717@gmail.com>
2023-08-09 15:56:33 -0300

    fix: test time zone in production

--- src/app/components/pdf/pdfView.tsx
+++ src/app/components/pdf/pdfView.tsx
@@ -3,8 +3,10 @@ import { PDFViewer } from "@react-pdf/renderer";
 import MyDocument from "./pdfDocumentComponent";
 import { format } from "date-fns";
 
-function convertTZ(date:Date, tzString:string) {
-  return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));
+function convertTZ(date: Date, tzString: string) {
+  const options = { timeZone: tzString, timeZoneName: 'short' };
+  const utcTime = date.getTime() + (date.getTimezoneOffset() * 60 * 1000);
+  return new Date(utcTime).toLocaleString("pt-BR", options);
 }
 
 
@@ -12,7 +14,7 @@ const PDFViewerComponent = ({ datas }: { datas: any }) => {
   return (
     <PDFViewer className="w-full h-full">
       <MyDocument datas={datas} date={
-        convertTZ(new Date(), 'America/Sao_Paulo').toLocaleString("pt-br") }  />
+        convertTZ(currentDateTime, 'America/Sao_Paulo') }  />
     </PDFViewer>
   );
 };
